0bfae6e273fa1218afcbc50d352a825ea28e5239
Brian Carlton <brian.carlton@ou.edu>
2019-02-21 15:21:36 -0600

    EquilateralTriangle 2019-02-21 15:21

--- src/EquilateralTriangle.java
+++ src/EquilateralTriangle.java
@@ -21,7 +21,9 @@ public class EquilateralTriangle extends Polygon
 	 */
 	public EquilateralTriangle(String id, double sideLength)
 	{
-		// TODO: complete this...
+		super(id);
+		this.sideLength = sideLength;
+		sideLengths.add(sideLength);
 	}
 
 	/**
@@ -32,7 +34,7 @@ public class EquilateralTriangle extends Polygon
 	@Override
 	public double getArea()
 	{
-		// TODO: complete this...
+		return sideLength*sideLength*Math.sqrt(3)/4;
 	}
 
 	/**
@@ -43,6 +45,6 @@ public class EquilateralTriangle extends Polygon
 	@Override
 	public String getShapeType()
 	{
-		// TODO: complete this...
+		return "EquilateralTriangle";
 	}
 }
